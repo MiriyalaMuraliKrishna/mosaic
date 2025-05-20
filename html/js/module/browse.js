97d0fa1c41c3598073731d447e8729b7a2c3834c
MuraliJbk <murali@jbkinfotech.com>
2025-05-20 15:27:38 +0530

    support page 111

--- html/js/module/browse.js
+++ html/js/module/browse.js
@@ -1,9 +1,19 @@
-class browse {
-  constructor(flag) {
+class Browses {
+  constructor(flag, ellipse, links) {
     this.flag = document.querySelector(flag);
+    this.browsellipse = document.querySelector(ellipse);
+    this.links = document.querySelectorAll(links);
+
+    // Bind methods to preserve `this`
+    this.eventHandler = this.eventHandler.bind(this);
+    this.browseHandler = this.browseHandler.bind(this);
   }
   init() {
     this.flag ? this.flag.addEventListener('click', this.eventHandler) : '';
+    console.log(this.links);
+    this.links.forEach((ele) => {
+      ele.addEventListener('mouseover', this.browseHandler);
+    });
   }
   // main logic here
   eventHandler(e) {
@@ -18,5 +28,17 @@ class browse {
       $ul.style.maxHeight = `${$ul.scrollHeight}px`;
     }
   }
+  browseHandler(e) {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const top = rect.top + window.scrollY - 533;
+    const left = rect.left + window.scrollX - 533;
+    console.log(top);
+    this.browsellipse.style.top = `${top}px`;
+    this.browsellipse.style.left = `${left}px`;
+  }
 }
-export const Browse = new browse('.resources-browse-mobile');
+export const Browse = new Browses(
+  '.resources-browse-mobile',
+  '.resources-browse-ellipse',
+  'ul.browse-links > li > a'
+);
