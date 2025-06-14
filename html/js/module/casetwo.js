55954798c1070f6c9e18b2a559e3d0b25cdc48bb
MiriyalaMuraliKrishna <muralikrishna.mca35@gmail.com>
2025-06-14 16:27:17 +0530

    tst

--- html/js/module/casetwo.js
+++ html/js/module/casetwo.js
@@ -4,9 +4,24 @@ class UsecaseTwo {
   }
   init() {
     if (!this.main) return;
-    let link = this.main.querySelector('ul.cases-two-links');
-    // event delegation starts now
-    console.log(link);
+    this.link = this.main.querySelector('ul.cases-two-links');
+    this.link.children[0].querySelector('a').classList.add('active');
+    this.link.addEventListener('click', this.eventHandler.bind(this));
+  }
+  eventHandler(e) {
+    if (e.target.tagName === 'A') {
+      e.preventDefault();
+      this.link.querySelectorAll('a').forEach((ele) => {
+        ele.classList.remove('active');
+      });
+      e.target.classList.add('active');
+      let list = this.main.querySelectorAll('.cases-two-list');
+      list.forEach((list) => {
+        list.style.display = 'none';
+      });
+      const id = e.target.getAttribute('href').trim().slice(1);
+      document.getElementById(id).style.display = 'block';
+    }
   }
 }
 export const usecase = new UsecaseTwo('.cases-two-main');
