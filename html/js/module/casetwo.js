6fcd017f84447d2b6e8b4b2487e3d261bf866afc
MuraliJbk <murali@jbkinfotech.com>
2025-06-16 11:56:11 +0530

    classrwo

--- html/js/module/casetwo.js
+++ html/js/module/casetwo.js
@@ -7,6 +7,8 @@ class UsecaseTwo {
     this.link = this.main.querySelector('ul.cases-two-links');
     this.link.children[0].querySelector('a').classList.add('active');
     this.link.addEventListener('click', this.eventHandler.bind(this));
+    this.mobile = this.main.querySelector('.cases-two-mobile');
+    this.mobile.addEventListener('click', this.eventHandlerMobile.bind(this));
   }
   eventHandler(e) {
     if (e.target.tagName === 'A') {
@@ -22,6 +24,34 @@ class UsecaseTwo {
       const id = e.target.getAttribute('href').trim().slice(1);
       document.getElementById(id).style.display = 'block';
     }
+  }
+  eventHandlerMobile(e) {
+    e.preventDefault();
+
+    let $target = e.currentTarget;
+    $target.classList.toggle('open');
+    let el = e.currentTarget;
+    if (el.dataset.mobile === 'true') {
+      this.link.style.maxHeight = `${this.link.scrollHeight}px`;
+      el.dataset.mobile = 'false';
+    } else {
+      this.link.style.maxHeight = `0px`;
+
+      el.dataset.mobile = 'true';
+    }
+
+    // h15f
+    this.link.addEventListener('click', function (e) {
+      if (e.target.tagName === 'A') {
+        e.preventDefault();
+        $target.classList.remove('open');
+
+        $target.querySelector('span.cases-two-mobile-txt').textContent =
+          e.target.textContent;
+        e.target.closest('ul.cases-two-links').style.maxHeight = '0px';
+      }
+    });
+    // h14f
   }
 }
 export const usecase = new UsecaseTwo('.cases-two-main');
