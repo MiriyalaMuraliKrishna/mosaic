0aeec3afffef79f401ad34d13d1a9c7c5a458b46
MuraliJbk <murali@jbkinfotech.com>
2025-06-19 12:09:21 +0530

    classrw headero

--- html/js/module/menu.js
+++ html/js/module/menu.js
@@ -3,8 +3,88 @@ export const Menu = {
   $nav: document.querySelector('.header_right'),
   $btn: document.querySelector('.humburger-btn'),
   $footerlinks: document.querySelectorAll('ul.foter-nav-links > li > a'),
+  $mainsolutions: document.querySelector(
+    'ul.main_menu > li.solutions-menu-item:not(.current-page-ancestor) > a'
+  ),
+  $solutions: document.querySelectorAll(
+    'ul.main_menu > li.solutions-menu-item > ul > li > a'
+  ),
+  $solutionsLi: document.querySelectorAll(
+    'ul.main_menu > li.solutions-menu-item > ul > li > ul > li.current_page_item'
+  ),
+  $mainproducts: document.querySelector(
+    'ul.main_menu > li.products-menu-item:not(.current-page-ancestor) > a'
+  ),
+  $products: document.querySelectorAll(
+    'ul.main_menu > li.products-menu-item > ul > li > a'
+  ),
+  $productsLi: document.querySelectorAll(
+    'ul.main_menu > li.products-menu-item > ul > li > ul > li.current_page_item'
+  ),
+  $mainmaterials: document.querySelector(
+    'ul.main_menu > li.materials-menu-item:not(.current-page-ancestor) > a'
+  ),
+  $materials: document.querySelectorAll(
+    'ul.main_menu > li.materials-menu-item > ul > li > a'
+  ),
+  $materialsLi: document.querySelectorAll(
+    'ul.main_menu > li.materials-menu-item > ul > li > ul > li.current_page_item'
+  ),
   init() {
     const _ = this;
+
+    // solutions hover
+    _.$mainsolutions && !_.$solutionsLi.length > 0
+      ? _.$solutions[0].parentElement.classList.add('open-menu')
+      : '';
+
+    _.$solutions.forEach((ele) => {
+      ele.addEventListener('mouseenter', function (e) {
+        _.$solutionsLi.forEach((li) =>
+          li.classList.remove('current_page_item')
+        );
+        _.$solutions.forEach((ele) =>
+          ele.parentElement.classList.remove('open-menu')
+        );
+        e.target.parentElement.classList.add('open-menu');
+      });
+    });
+    // solutions hover
+
+    // products hover
+    _.$mainproducts && !_.$productsLi.length > 0
+      ? _.$products[0].parentElement.classList.add('open-menu')
+      : '';
+
+    _.$products.forEach((ele) => {
+      ele.addEventListener('mouseenter', function (e) {
+        _.$productsLi.forEach((li) => li.classList.remove('current_page_item'));
+        _.$products.forEach((ele) =>
+          ele.parentElement.classList.remove('open-menu')
+        );
+        e.target.parentElement.classList.add('open-menu');
+      });
+    });
+    // products hover
+
+    // materials hover
+    _.$mainmaterials && !_.$materialsLi.length > 0
+      ? _.$materials[0].parentElement.classList.add('open-menu')
+      : '';
+
+    _.$materials.forEach((ele) => {
+      ele.addEventListener('mouseenter', function (e) {
+        _.$materialsLi.forEach((li) =>
+          li.classList.remove('current_page_item')
+        );
+        _.$materials.forEach((ele) =>
+          ele.parentElement.classList.remove('open-menu')
+        );
+        e.target.parentElement.classList.add('open-menu');
+      });
+    });
+    // materials hover
+
     if (!_.$ele || !_.$btn) return;
     // scrolly with
     _.$btn.addEventListener('click', (e) => {
