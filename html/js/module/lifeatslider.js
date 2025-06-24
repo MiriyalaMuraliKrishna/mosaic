bbdc0f1fcc75cbf0ee3e74d019eb35b91e237009
MuraliJbk <murali@jbkinfotech.com>
2025-06-24 17:26:34 +0530

    classrw headero

--- html/js/module/lifeatslider.js
+++ html/js/module/lifeatslider.js
@@ -8,36 +8,27 @@ export const lifeatslider = {
     const item = this.lifeateele;
     if (!item) return;
 
-    // Animate thumbnails
-    const thumb = document.querySelectorAll('[data-animate*="life-at-anime"]');
-    thumb.forEach((image, index) => {
-      image.style.animationDelay = `${index * 100}ms`;
-    });
+    const autoScrollSpeed = item.dataset.speed
+      ? parseFloat(item.dataset.speed)
+      : 0.5;
 
-    setTimeout(() => {
-      const autoScrollSpeed = item.dataset.speed
-        ? parseFloat(item.dataset.speed)
-        : 0.5;
+    const direction = item.dataset.direction === 'reversed' ? -1 : 1;
 
-      const direction = item.dataset.direction === 'reversed' ? -1 : 1;
-
-      const splide = new Splide(item, {
-        pagination: false,
-        arrows: false,
-        direction: 'ltr',
-        type: 'loop',
-
-        autoWidth: true,
-        autoScroll: {
-          speed:
-            item.hasAttribute('data-direction') &&
-            $(this).data('direction') === 'reversed'
-              ? autoScrollSpeed * -1
-              : autoScrollSpeed,
-          pauseOnHover: false,
-        },
-        extensions: { AutoScroll },
-      }).mount({ AutoScroll });
-    }, 1000);
+    const splide = new Splide(item, {
+      pagination: false,
+      arrows: false,
+      direction: 'ltr',
+      type: 'loop',
+      autoWidth: true,
+      autoScroll: {
+        speed:
+          item.hasAttribute('data-direction') &&
+          $(this).data('direction') === 'reversed'
+            ? autoScrollSpeed * -1
+            : autoScrollSpeed,
+        pauseOnHover: false,
+      },
+      extensions: { AutoScroll },
+    }).mount({ AutoScroll });
   },
 };
