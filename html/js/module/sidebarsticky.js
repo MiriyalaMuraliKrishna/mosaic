2c1f9abe47c7765774b6ddd9e86cd54cb84f4a1f
MuraliJbk <murali@jbkinfotech.com>
2025-07-14 12:33:03 +0530

    arrow css

--- html/js/module/sidebarsticky.js
+++ html/js/module/sidebarsticky.js
@@ -1,60 +1,123 @@
+// import 'css-element-queries/src/ResizeSensor';
+// import StickySidebar from 'sticky-sidebar';
+
+// function Sidebar(selector) {
+//   this.header = document.querySelector('header.site-header');
+//   this.ele = document.querySelectorAll(selector);
+//   this.instances = []; // store sidebar instances
+// }
+
+// Sidebar.prototype.init = function () {
+//   const media = window.matchMedia('(min-width: 1024px)');
+
+//   const checkAndInit = (matches) => {
+//     if (matches) {
+//       this.initSticky();
+//     } else {
+//       this.destroySticky();
+//     }
+//   };
+
+//   checkAndInit(media.matches);
+
+//   media.addEventListener('change', (e) => {
+//     checkAndInit(e.matches);
+//   });
+// };
+
+// Sidebar.prototype.initSticky = function () {
+//   if (!this.header) return;
+//   const height = this.header.getBoundingClientRect().height;
+
+//   this.ele.forEach((ele) => {
+//     if (!ele || ele.classList.contains('sticky-initialized')) return;
+
+//     const instance = new StickySidebar(ele, {
+//       topSpacing: height,
+//       bottomSpacing: 20,
+//       containerSelector: '.alt-stick-section',
+//       innerWrapperSelector: '.alt-stick-text',
+//       resizeSensor: true,
+//     });
+
+//     ele.classList.add('sticky-initialized');
+//     this.instances.push(instance);
+//   });
+// };
+
+// Sidebar.prototype.destroySticky = function () {
+//   this.instances.forEach((instance) => {
+//     instance.destroy();
+//   });
+
+//   this.ele.forEach((ele) => {
+//     ele.classList.remove('sticky-initialized');
+//   });
+
+//   this.instances = [];
+// };
+
+// export const sidebar = new Sidebar('.alt-stick-left');
+
 import 'css-element-queries/src/ResizeSensor';
 import StickySidebar from 'sticky-sidebar';
 
-function Sidebar(selector) {
-  this.header = document.querySelector('header.site-header');
-  this.ele = document.querySelectorAll(selector);
-  this.instances = []; // store sidebar instances
-}
+export const sidebar = {
+  selector: '.alt-stick-left',
+  header: null,
+  ele: null,
+  instances: [],
 
-Sidebar.prototype.init = function () {
-  const media = window.matchMedia('(min-width: 1024px)');
+  init() {
+    this.header = document.querySelector('header.site-header');
+    this.ele = document.querySelectorAll(this.selector);
 
-  const checkAndInit = (matches) => {
-    if (matches) {
-      this.initSticky();
-    } else {
-      this.destroySticky();
-    }
-  };
+    const media = window.matchMedia('(min-width: 1024px)');
 
-  checkAndInit(media.matches);
+    const checkAndInit = (matches) => {
+      if (matches) {
+        this.initSticky();
+      } else {
+        this.destroySticky();
+      }
+    };
 
-  media.addEventListener('change', (e) => {
-    checkAndInit(e.matches);
-  });
-};
+    checkAndInit(media.matches);
 
-Sidebar.prototype.initSticky = function () {
-  if (!this.header) return;
-  const height = this.header.getBoundingClientRect().height;
+    media.addEventListener('change', (e) => {
+      checkAndInit(e.matches);
+    });
+  },
 
-  this.ele.forEach((ele) => {
-    if (!ele || ele.classList.contains('sticky-initialized')) return;
+  initSticky() {
+    if (!this.header) return;
+    const height = this.header.getBoundingClientRect().height;
 
-    const instance = new StickySidebar(ele, {
-      topSpacing: height,
-      bottomSpacing: 20,
-      containerSelector: '.alt-stick-section',
-      innerWrapperSelector: '.alt-stick-text',
-      resizeSensor: true,
+    this.ele.forEach((ele) => {
+      if (!ele || ele.classList.contains('sticky-initialized')) return;
+
+      const instance = new StickySidebar(ele, {
+        topSpacing: height,
+        bottomSpacing: 20,
+        containerSelector: '.alt-stick-section',
+        innerWrapperSelector: '.alt-stick-text',
+        resizeSensor: true,
+      });
+
+      ele.classList.add('sticky-initialized');
+      this.instances.push(instance);
+    });
+  },
+
+  destroySticky() {
+    this.instances.forEach((instance) => {
+      instance.destroy();
     });
 
-    ele.classList.add('sticky-initialized');
-    this.instances.push(instance);
-  });
+    this.ele.forEach((ele) => {
+      ele.classList.remove('sticky-initialized');
+    });
+
+    this.instances = [];
+  },
 };
-
-Sidebar.prototype.destroySticky = function () {
-  this.instances.forEach((instance) => {
-    instance.destroy();
-  });
-
-  this.ele.forEach((ele) => {
-    ele.classList.remove('sticky-initialized');
-  });
-
-  this.instances = [];
-};
-
-export const sidebar = new Sidebar('.alt-stick-left');
